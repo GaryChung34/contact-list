5ba0ad43f4e33d388f34f704732039d8dbef5123
Gary Chung <garychung@ps.asl.com.hk>
2022-10-05 02:48:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+    Avatar,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemText,
+    TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [contactList, setContactList] = useState({});
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+                setContactList(result);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="navBar">
+                    <div className="navBarHeader">Rick and Morty</div>
+                    <div className="navBarContact">Contact</div>
+                </div>
+                <div className="contactList">
+                    <div className="contactListHeader">
+                        <div className="title">Contact list</div>
+                        <TextField
+                            id="search-bar"
+                            variant="standard"
+                            size="small"
+                            placeholder="search any name"
+                        />
+                    </div>
+                    <div className="contactListContent">
+                        <List>
+                            <ListItem disablePadding>
+                                <ListItemButton>
+                                    <ListItemText primary="abc" />
+                                </ListItemButton>
+                            </ListItem>
+                            {contactList?.results?.map((item) => (
+                                <ListItem disablePadding>
+                                    <ListItemButton>
+                                        <ListItemAvatar>
+                                            <Avatar
+                                                alt={item?.name}
+                                                src={item?.image}
+                                            />
+                                        </ListItemAvatar>
+                                        <ListItemText primary={item?.name} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </div>
+                <div className="infoSection">Info section</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
